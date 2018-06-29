d090ea0d3bd22439590bf84bb4c8e0b62635c763
bgoldbeck <bgoldbeck21@gmail.com>
2018-06-29 10:48:34 -0700

    Change folder structure

---
+++ src/game/components/Player.java
@@ -0,0 +1,22 @@
+package game.components;
+
+import ecs.Component;
+
+public class Player extends Component {
+    public String name = "";
+
+    public void start() {
+        System.out.println("Player started ");
+        return;
+    }
+
+    public void update() {
+        System.out.println("Player updated");
+        return;
+    }
+
+    public void render() {
+        System.out.println("Player rendered");
+        return;
+    }
+}
