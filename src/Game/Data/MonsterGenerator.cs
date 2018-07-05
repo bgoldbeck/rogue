29d3b9f05f0eff7f2993a5b5dd3b9a62eb06e24e
DanielRBramblett <DanielRBramblett@users.noreply.github.com>
2018-07-05 16:22:31 -0700

    Wrote comments for MonsterGenerator class
    Didn't change the functionality of the class. Added the copyright to the file along with comments on all the functions in the class.

--- src/Game/Data/MonsterGenerator.cs
+++ src/Game/Data/MonsterGenerator.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//Copyright(c) 2018 Daniel Bramblett, Daniel Dupriest, Brandon Goldbeck
+
+using System;
 using Game.Components;
 using Ecs;
 
@@ -6,6 +8,13 @@ namespace Game.Data
 {
     class MonsterGenerator
     {
+        /// <summary>
+        /// This function generates an instance of Enemy using the level and fills it in for an enemy type snake. 
+        /// It also returns the model of the enemy using a reference of an array of strings.
+        /// </summary>
+        /// <param name="level"></param>
+        /// <param name="model"></param>
+        /// <returns></returns>
         static private Enemy SnakeGenerator(int level, string[] model)
         {
             model[0] = "s";                         //Monster's model
@@ -17,6 +26,14 @@ namespace Game.Data
                              level                  //Equation for the monster's attack.
                              );
         }
+
+        /// <summary>
+        /// This function generates an instance of Enemy using the level and fills it in for an enemy type goblin. 
+        /// It also returns the model of the enemy using a reference of an array of strings.
+        /// </summary>
+        /// <param name="level"></param>
+        /// <param name="model"></param>
+        /// <returns></returns>
         static private Enemy GoblinGenerator(int level, string[] model)
         {
             model[0] = "g";                             //Monster's model
@@ -29,26 +46,34 @@ namespace Game.Data
                             );
         }
 
-        //This function was inspired by this discussion on Stack Overflow:
+        //This function and delegate were inspired by this discussion on Stack Overflow:
         //https://stackoverflow.com/questions/3767942/storing-a-list-of-methods-in-c-sharp
-
         delegate Enemy spawnGenerator(int l, string[] s);
+        /// <summary>
+        /// This function takes in a reference to the Random class, a level, and a GameObject
+        /// and randomly selects a monster and generates the enemy.
+        /// </summary>
+        /// <param name="rand"></param>
+        /// <param name="level"></param>
+        /// <param name="slot"></param>
         static public void Fill(Random rand, int level, GameObject slot)
         {
             string[] model = new string[1];
             model[0] = "!";
+            //Adds a Model component to the game object passed in.
             Model mod = (Model)slot.AddComponent(new Model());
 
+            //Generates an array of methods for each monster type.
             spawnGenerator[] generatorArr = new spawnGenerator[]
             {
                 (param1, param2) => SnakeGenerator(param1, param2),
                 (param1, param2) => GoblinGenerator(param1, param2),
             };
          
+            //Using the passed in Random instance, a random monster is picked and the
+            //information on the enemy is filled in.
             int value = rand.Next() % generatorArr.Length;
-
             slot.AddComponent(generatorArr[value](level, model));
-            //System.Console.WriteLine("--> " +model[0] + "\n");
             mod.model.Add(model[0]);
         }
     }
