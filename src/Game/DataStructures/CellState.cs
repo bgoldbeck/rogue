8cbf8a4cb407982588702b7bcbbafb297c34689a
DanielRBramblett <DanielRBramblett@users.noreply.github.com>
2018-07-05 16:55:13 -0700

    Wrote comments for the enumerations
    Wrote comments when and what each enumeration is used.

--- src/Game/DataStructures/CellState.cs
+++ src/Game/DataStructures/CellState.cs
@@ -1,8 +1,9 @@
 ﻿//Copyright(c) 2018 Daniel Bramblett, Daniel Dupriest, Brandon Goldbeck
 
-using System;
-
 namespace Game.DataStructures
 {
+    /// <summary>
+    /// An enumeration used by the Map class to mark grid spaces as walls or not.
+    /// </summary>
     public enum CellState { Open, Blocked };
 }
