2f055c6880a932ad1dadde8bab9186d725516ebe
DanielRBramblett <DanielRBramblett@users.noreply.github.com>
2018-07-09 15:11:45 -0700

    Added Health display to the console

--- src/Game/Components/HUD.cs
+++ src/Game/Components/HUD.cs
@@ -80,6 +80,7 @@ namespace Game.Components
             AddTop(model);
             AddText(model, player.Name);
             AddText(model, "Level: " + player.Level);
+            AddText(model, "Health: " + player.HitPoints);
             AddText(model, "Attack: " + player.Attack);
             AddText(model, "Armor: " + player.Armor);
             AddDivider(model);
