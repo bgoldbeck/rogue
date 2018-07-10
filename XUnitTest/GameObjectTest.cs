0244ec6c9ce3da5ba2f6140913475ea844ac73ad
bgoldbeck21@gmail.com <bgoldbeck21@gmail.com>
2018-07-10 12:48:18 -0700

    Adding unit test project

---
+++ XUnitTest/GameObjectTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace XUnitTestProject
+{
+    public class GameObjectTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
