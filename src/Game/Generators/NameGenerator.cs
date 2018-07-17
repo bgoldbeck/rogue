79d8d3453a9bfdfddb7e8f53800549e908613edb
kououken <kououken@gmail.com>
2018-07-17 13:00:24 -0700

    Set up stub class for generating names.

---
+++ src/Game/Generators/NameGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.Generators
+{
+    class NameGenerator
+    {
+        private static String GetNameStartingWith(char character)
+        {
+            return " ";
+        }
+    }
+}
