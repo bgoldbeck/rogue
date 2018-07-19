4702eefa123b91df108da8d3de283f19531dd380
DanielRBramblett <bram4@pdx.edu>
2018-07-19 11:22:34 -0700

    Added methods to move objects on graph

--- src/Game/Components/NavigatorMap.cs
+++ src/Game/Components/NavigatorMap.cs
@@ -21,27 +21,15 @@ namespace Game.Components
         {
             graph = new Graph<Vec2i>();
             Map map = Map.CacheInstance();
-            if(map != null)
+            if (map != null)
             {
                 // Building the graph from the map.
                 for (int x = 0; x < map.width; ++x)
                 {
                     for (int y = 0; y < map.height; ++y)
                     {
-                        // TODO. need to block going off the maps edges.
-                        //if (map.PeekObject(x,y) != null) { continue; }
                         Vec2i from = new Vec2i(x, y);
-                        Vec2i[] neighbors = new Vec2i[]
-                        {
-                        new Vec2i(x + 1, y), new Vec2i(x - 1, y), new Vec2i(x, y + 1), new Vec2i(x, y - 1)
-                        };
-                        foreach (Vec2i currentNeighbor in neighbors)
-                        {
-                            if (map.PeekObject(currentNeighbor) == null || map.PeekObject(currentNeighbor).GetComponent<Door>() != null)
-                            {
-                                graph.AddEdge(from, currentNeighbor);
-                            }
-                        }
+                        AddNeighbors(from, map);
                     }
                 }
             }
@@ -52,5 +40,49 @@ namespace Game.Components
             return graph;
         }
 
+        public static bool MoveObject(Vec2i currentPosition, Vec2i newPosition)
+        {
+            Map map = Map.CacheInstance();
+            if (graph == null || map == null)
+            {
+                return false;
+            }
+
+            AddNeighbors(currentPosition, map);
+            return false;
+        }
+
+        public static void AddNeighbors(Vec2i from, Map map)
+        {
+            if (graph == null)
+            {
+                return;
+            }
+            Vec2i[] neighbors = new Vec2i[]
+            {
+                        new Vec2i(from.x + 1, from.y), new Vec2i(from.x - 1, from.y), new Vec2i(from.x, from.y + 1), new Vec2i(from.x, from.y - 1)
+            };
+            foreach (Vec2i currentNeighbor in neighbors)
+            {
+                // TODO. need to block going off the maps edges.
+                //if (map.PeekObject(x,y) != null) { continue; }
+                if (map.PeekObject(currentNeighbor) == null || map.PeekObject(currentNeighbor).GetComponent<Door>() != null)
+                {
+                    graph.AddEdge(from, currentNeighbor);
+                }
+            }
+        }
+        public static void RemoveNeighbors(Vec2i from)
+        {
+            if (graph == null)
+            {
+                return;
+            }
+
+            foreach(Vec2i neighbor in graph.GetEdges(from))
+            {
+                graph.RemoveEdge(from, neighbor);
+            }
+        }
     }
 }
