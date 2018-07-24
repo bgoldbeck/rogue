c4ac06852c740ed5f068b95e427257dec6ff97cf
bgoldbeck <bgoldbeck21@gmail.com>
2018-07-23 20:27:55 -0700

    fixed more unit tests

--- XUnitTest/Ecs/TimerTests.cs
+++ XUnitTest/Ecs/TimerTests.cs
@@ -10,43 +10,44 @@ namespace XUnitTests
 {
     public class TimerTests
     {
-        public static bool calledBack = false;
+        public static bool calledBack1 = false;
+        public static bool calledBack2 = false;
 
         internal void Callback1()
         {
-            calledBack = true;
+            calledBack1 = true;
             return;
         }
 
         internal void Callback2()
         {
-            calledBack = true;
+            calledBack2 = true;
             return;
         }
 
         [Fact]
         public void TimerCallsCallback()
         {
-            calledBack = false;
+            calledBack1 = false;
             Time.Update();
             TimerKeeper.AddTimer(0.1f, Callback1);
             System.Threading.Thread.Sleep(500);
             Time.Update();
             TimerKeeper.Update();
-            Assert.True(calledBack);
+            Assert.True(calledBack1);
             return;
         }
 
         [Fact]
         public void TimerShouldntCallCallback()
         {
-            calledBack = false;
+            calledBack2 = false;
             Time.Update();
             TimerKeeper.AddTimer(1.0f, Callback2);
             System.Threading.Thread.Sleep(10);
             Time.Update();
             TimerKeeper.Update();
-            Assert.False(calledBack);
+            Assert.False(calledBack2);
             return;
         }
     }
