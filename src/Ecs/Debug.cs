489ce0144b9a87c5db66b9c7f335eb5706dff9b5
bgoldbeck <bgoldbeck21@gmail.com>
2018-08-03 13:09:55 -0700

    Moving Debug to Ecs namespace. Deleting TimerKeeper.

--- src/Ecs/Debug.cs
+++ src/Ecs/Debug.cs
@@ -3,29 +3,49 @@
 using System;
 using System.IO;
 
-namespace IO
+namespace Ecs
 {
+    /// <summary>
+    /// Writes debug information to a log file.
+    /// </summary>
     class Debug
     {
         private static StreamWriter writer = null;
 
+        /// <summary>
+        /// Write to log using normal debug flag.
+        /// </summary>
+        /// <param name="log">The log message to write.</param>
         public static void Log(string log)
         {
             WriteToLog(string.Format("{0} {1} {2}", System.DateTime.Now.ToString(), "DEBUG:", log));
             return;
         }
+
+        /// <summary>
+        /// Write to log using the warning flag.
+        /// </summary>
+        /// <param name="log">The log message to write.</param>
         public static void LogWarning(string warning)
         {
             WriteToLog(string.Format("{0} {1} {2}", System.DateTime.Now.ToString(), "Warning:", warning));
             return;
         }
 
+        /// <summary>
+        /// Write to log using the error flag.
+        /// </summary>
+        /// <param name="log">The log message to write.</param>
         public static void LogError(string error)
         {
             WriteToLog(string.Format("{0} {1} {2}", System.DateTime.Now.ToString(), "ERROR:", error));
             return;
         }
 
+        /// <summary>
+        /// Write the message to the log file.
+        /// </summary>
+        /// <param name="message"></param>
         [System.Diagnostics.Conditional("DEBUG")]
         private static void WriteToLog(string message)
         {
@@ -41,6 +61,9 @@ namespace IO
             return;
         }
 
+        /// <summary>
+        /// Create the log file.
+        /// </summary>
         private static void CreateLogFile()
         {
             File.WriteAllText("Log.txt", String.Empty);
