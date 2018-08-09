91b72e63914220b39bfa5b09f7d27621bb38bc73
bgoldbeck <bgoldbeck21@gmail.com>
2018-08-08 21:00:07 -0700

    Updated docs

--- docs/search/all_a.js
+++ docs/search/all_a.js
@@ -3,6 +3,7 @@ var searchData=
   ['map',['Map',['../class_game_1_1_components_1_1_map.html',1,'Game::Components']]],
   ['mapmanager',['MapManager',['../class_game_1_1_components_1_1_map_manager.html',1,'Game.Components.MapManager'],['../class_game_1_1_components_1_1_map_manager.html#a2484aeb4a1294507739d329db6cb4b68',1,'Game.Components.MapManager.MapManager()'],['../class_game_1_1_components_1_1_map_manager.html#ada74da92223a244a7fcdce387c2af503',1,'Game.Components.MapManager.MapManager(int newWidth, int newHeight, int newLevel)']]],
   ['maptile',['MapTile',['../class_game_1_1_components_1_1_map_tile.html',1,'Game::Components']]],
+  ['millisecondstoseconds',['MillisecondsToSeconds',['../class_ecs_1_1_time.html#a351b0978a1802f4bdead765238091ee0',1,'Ecs::Time']]],
   ['minotaur',['Minotaur',['../class_game_1_1_components_1_1_enemy_types_1_1_minotaur.html',1,'Game::Components::EnemyTypes']]],
   ['model',['Model',['../class_game_1_1_components_1_1_model.html',1,'Game::Components']]]
 ];
--- docs/search/functions_a.js
+++ docs/search/functions_a.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['mapmanager',['MapManager',['../class_game_1_1_components_1_1_map_manager.html#a2484aeb4a1294507739d329db6cb4b68',1,'Game.Components.MapManager.MapManager()'],['../class_game_1_1_components_1_1_map_manager.html#ada74da92223a244a7fcdce387c2af503',1,'Game.Components.MapManager.MapManager(int newWidth, int newHeight, int newLevel)']]]
+  ['mapmanager',['MapManager',['../class_game_1_1_components_1_1_map_manager.html#a2484aeb4a1294507739d329db6cb4b68',1,'Game.Components.MapManager.MapManager()'],['../class_game_1_1_components_1_1_map_manager.html#ada74da92223a244a7fcdce387c2af503',1,'Game.Components.MapManager.MapManager(int newWidth, int newHeight, int newLevel)']]],
+  ['millisecondstoseconds',['MillisecondsToSeconds',['../class_ecs_1_1_time.html#a351b0978a1802f4bdead765238091ee0',1,'Ecs::Time']]]
 ];
